963c3bbfde30d34edc578798de78e0f0ff11797e
AndreyKhoroshev <andrey.khoroshev@myy.haaga-helia.fi>
2026-02-03 23:13:06 +0200

    Entity added

--- src/main/java/hh/backend/bookstore/domain/Book.java
+++ src/main/java/hh/backend/bookstore/domain/Book.java
@@ -1,6 +1,16 @@
 package hh.backend.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class Book {
+ @Id
+ @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
     private String title;
     private String author;
@@ -50,6 +60,14 @@ public class Book {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
 }
